0a3b1fc2cb940e6fd3ecfb1148b27182b0d2a9e2
Michael <michael-bln@protonmail.com>
2017-08-07 17:08:34 +0200

    Disable html minification (was killing value converters)

--- aurelia_project/tasks/process-markup.js
+++ aurelia_project/tasks/process-markup.js
@@ -1,5 +1,4 @@
 import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
 import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
@@ -7,11 +6,5 @@ import {build} from 'aurelia-cli';
 export default function processMarkup() {
   return gulp.src(project.markupProcessor.source)
     .pipe(changedInPlace({firstPass:true}))
-    .pipe(htmlmin({
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true
-    }))
     .pipe(build.bundle());
 }
